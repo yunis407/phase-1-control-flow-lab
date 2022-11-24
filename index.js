860de95844608393f1b310c714d1ab82145ea98e
yunisaliosman <yunis.osman@student.moringaschool.com>
2022-11-24 12:22:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+// Write your code here!
+  if(ride <= 400) {
+    return 'this one is on me!' ;
+  }else if (ride > 2000 && ride <= 2500){
+    return 'I will gladly take your thirty bucks.' ;
+  }else{
+    return 'No can do.' ;
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return(city == 'NYC'? 'Ok,sounds good.' : 'No go.') ;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  swith(tip) {
+    case "generous" ;
+    return("Thank you so much.")
+    break;
+    case "not as generous" ;
+      return("Thank you.")
+      break;
+      default;
+        return('Bye.')
+  }
 }
